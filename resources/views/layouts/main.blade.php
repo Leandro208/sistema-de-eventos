ab3e0dfbdaf7c5691fdbea4250bf0bea21618adb
Leandro Barbosa <boyleandro_208@hotmail.com>
2023-03-19 08:14:02 -0300

    feat: Criando login/registro

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,11 +20,15 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="collapse navbar-collapse" id="navbar">
-                <a href="/" class="navbar-brand">
-                    <img src="/img/events_logo.svg" alt="Sistema de Eventos">
-                </a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <a href="/" class="navbar-brand">
+                <img src="/img/events_logo.svg" alt="Sistema de Eventos">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a href="/" class="nav-link">Eventos</a>
@@ -32,12 +36,27 @@
                     <li class="nav-item">
                         <a href="/events/create" class="nav-link">Criar Eventos</a>
                     </li>
+                    @auth
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Entrar</a>
+                        <a href="/dashboard" class="nav-link">Meus Eventos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/" class="nav-link">Cadastrar</a>
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <a href="/logout" class="nav-link" onclick="event.preventDefault();
+                                this.closest('form').submit();">Sair</a>
+                        </form>
                     </li>
+                    @endauth
+
+                    @guest
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/register" class="nav-link">Cadastrar</a>
+                    </li>
+                    @endguest
                 </ul>
             </div>
         </nav>
@@ -46,7 +65,7 @@
         <div class="container-fluid">
             <div class="row">
                 @if(session('msg'))
-                    <p class="msg">{{ session('msg') }}</p>
+                <p class="msg">{{ session('msg') }}</p>
                 @endif
                 @yield('content')
             </div>
@@ -56,6 +75,7 @@
         <p>Desenvolvido por Leandro Barbosa &copy; 2023</p>
     </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
