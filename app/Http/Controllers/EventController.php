900ef041f25420cda4f995c68b06b5e31eaa1dfb
Leandro Barbosa <boyleandro_208@hotmail.com>
2023-03-19 11:37:12 -0300

    feat: criando uma dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -51,6 +52,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -60,6 +64,17 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event'=>$event]);
+        $eventOwner = user::where('id', $event->user_id)
+        ->first()->toArray();
+
+        return view('events.show', ['event'=>$event, 'eventOwner'=>$eventOwner]);
+    }
+
+    public function dashboard(){
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events'=>$events]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 
-Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 
 Route::post('/events', [EventController::class, 'store']);
 
@@ -27,14 +27,4 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
